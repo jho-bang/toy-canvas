2789040678e67db5574e7d9b88dc85155580ae04
Jaeho Bang <jhb@marpple.com>
2024-06-20 18:58:54 +0900

    show hide

--- src/pages/WorkSpace/WorkSpace.page.ts
+++ src/pages/WorkSpace/WorkSpace.page.ts
@@ -17,23 +17,22 @@ export class WorkSpacePage extends Page<Props> {
   rightPanelView = new RightPanelView({});
   leftPanelView = new LeftPanelView({});
 
-  @on("mousedown")
-  private _mouseDown(ev: MouseEvent) {
-    this.isShow = !this.isShow;
-
-    if (this.isShow) {
-      this.topPanelView.show("top");
-      this.leftPanelView.show("left");
-      this.rightPanelView.show("right");
-    } else {
-      this.topPanelView.hide("top");
-      this.leftPanelView.hide("left");
-      this.rightPanelView.hide("right");
+  @on("keydown")
+  private _showHide(ev: KeyboardEvent) {
+    if (ev.metaKey && ev.key === "/") {
+      this.isShow = !this.isShow;
+      this.topPanelView[this.isShow ? "show" : "hide"]("top");
+      this.leftPanelView[this.isShow ? "show" : "hide"]("left");
+      this.rightPanelView[this.isShow ? "show" : "hide"]("right");
     }
   }
 
+  override onRender() {
+    this.element().focus();
+  }
+
   override template() {
-    return html`<div class="${style.workspace}">
+    return html`<div class="${style.workspace}" tabindex="0">
       ${this.topPanelView} ${this.leftPanelView} ${this.rightPanelView}
       ${this.canvasView}
     </div>`;
